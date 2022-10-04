5fd57ae2df16861aeda8105945e9ffdee6512b2e
Emerson452 <emerson452pro@gmail.com>
2022-10-04 19:39:38 +0200

    📱 🖥️ Responsve mode for desktop

--- src/App.js
+++ src/App.js
@@ -1,31 +1,63 @@
-import Header from './components/header';
-import Boutton from './components/boutton';
-import Description from './components/description';
-import Slider from './components/slider';
-import Description_Promotion from './components/description_promotion';
-import Boutton_Information from './components/boutton_information';
-import Map from './components/map';
-import Horraires from './components/horraires';
-import Socials from './components/socials';
-import Footer from './components/footer';
+import useMatchMedia from "react-use-match-media";
 
+import Header from "./components/header";
+import Boutton from "./components/boutton";
+import Description from "./components/description";
+import Slider from "./components/slider";
+import Description_Promotion from "./components/description_promotion";
+import Boutton_Information from "./components/boutton_information";
+import Map from "./components/map";
+import Horraires from "./components/horraires";
+import Socials from "./components/socials";
+import Footer from "./components/footer";
+import Photo_Grid from "./components/photo_grid";
 
 function App() {
+  const isWideViewport = useMatchMedia("(max-width:700px)");
 
   return (
-    <div className="App">
-
-      <Header></Header>
-      <Boutton></Boutton>
-      <Description></Description>
-      <Slider></Slider>
-      <Description_Promotion></Description_Promotion>
-      <Boutton_Information></Boutton_Information>
-      <Map></Map>
-      <Horraires></Horraires>
-      <Socials></Socials>
-      <Footer></Footer>
-
+    <div>
+      {isWideViewport ? (
+        <div className="App">
+          <Header></Header>
+          <Boutton></Boutton>
+          <Description></Description>
+          <Slider></Slider>
+          <Description_Promotion></Description_Promotion>
+          <Boutton_Information></Boutton_Information>
+          <Map></Map>
+          <Horraires></Horraires>
+          <Socials></Socials>
+          <Footer></Footer>
+        </div>
+      ) : (
+        <div className="app-desktop">
+          <div className="container-top-desktop">
+            <Header></Header>
+            <Boutton></Boutton>
+          </div>
+          <div className="container-mid-desktop">
+            <div className="container-map-desktop">
+              <Map></Map>
+            </div>
+            <div className="container-description-desktop">
+              <Description></Description>
+              <Description_Promotion></Description_Promotion>
+              <Boutton_Information></Boutton_Information>
+              <Horraires></Horraires>
+            </div>
+          </div>
+          <div className="container-bot-desktop">
+            <div className="container-photogrid-desktop">
+              <Photo_Grid></Photo_Grid>
+              </div>
+              <div className="container-footer-desktop">
+              <Footer></Footer>
+              <Socials></Socials>
+              </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/socials/index.js
+++ src/components/socials/index.js
@@ -1,22 +1,32 @@
-import './index.css'
-import vector_2_socials from'../assets/Vector_2.png';
-import image_3_socials from'../assets/Vector_3.png';
+import "./index.css";
+import vector_2_socials from "../assets/Vector_2.png";
+import image_3_socials from "../assets/Vector_3.png";
 
 function Socials() {
-
-	return (
-	    <div className="socials">
-            <p>Suivez-nous</p>
-            <div className='container-reseaux'>
-                <div className='Social'>
-                    <img src={vector_2_socials} className="image_2_socials" alt="logo_facebook" />
-                </div>
-                <div className='Social'>
-                    <img src={image_3_socials} className="image_3_socials" alt="logo_instagram" />
-                </div>
-            </div>
+  return (
+    <div className="socials">
+      <div className="text-socials">
+        <p id="plus-de-photos">Plus de photos</p>
+        <p>Suivez-nous</p>
       </div>
-	)
+      <div className="container-reseaux">
+        <div className="Social">
+          <img
+            src={vector_2_socials}
+            className="image_2_socials"
+            alt="logo_facebook"
+          />
+        </div>
+        <div className="Social">
+          <img
+            src={image_3_socials}
+            className="image_3_socials"
+            alt="logo_instagram"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Socials
+export default Socials;
