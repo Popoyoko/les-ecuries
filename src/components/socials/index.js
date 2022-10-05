70318fd66cdd5c68f5d9489db43c9222710749d0
Emerson452 <emerson452pro@gmail.com>
2022-10-05 19:04:30 +0200

    :art: improvement of the social container

--- src/components/socials/index.js
+++ src/components/socials/index.js
@@ -1,30 +1,57 @@
 import "./index.css";
 import vector_2_socials from "../assets/Vector_2.png";
 import image_3_socials from "../assets/Vector_3.png";
+import useMatchMedia from "react-use-match-media";
 
 function Socials() {
+  const isWideViewport = useMatchMedia("(max-width:700px)");
+
   return (
     <div className="socials">
-      <div className="text-socials">
-        <p id="plus-de-photos">Plus de photos</p>
-        <p>Suivez-nous</p>
-      </div>
-      <div className="container-reseaux">
-        <div className="Social">
-          <img
-            src={vector_2_socials}
-            className="image_2_socials"
-            alt="logo_facebook"
-          />
+      {isWideViewport ? (
+        <div className="split-socials">
+          <p>Suivez-nous</p>
+          <div className="split-socials-smartphone">
+            <div className="social">
+              <img
+                src={vector_2_socials}
+                className="image_2_socials"
+                alt="logo_facebook"
+              />
+            </div>
+            <div className="social">
+              <img
+                src={image_3_socials}
+                className="image_3_socials"
+                alt="logo_instagram"
+              />
+            </div>
+          </div>
         </div>
-        <div className="Social">
-          <img
-            src={image_3_socials}
-            className="image_3_socials"
-            alt="logo_instagram"
-          />
+      ) : (
+        <div className="container-socials">
+          <div className="split-socials">
+            <p id="plus-de-photos">Plus de photos</p>
+            <div className="social">
+              <img
+                src={vector_2_socials}
+                className="image_2_socials"
+                alt="logo_facebook"
+              />
+            </div>
+          </div>
+          <div className="split-socials">
+            <p>Suivez-nous</p>
+            <div className="social">
+              <img
+                src={image_3_socials}
+                className="image_3_socials"
+                alt="logo_instagram"
+              />
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
