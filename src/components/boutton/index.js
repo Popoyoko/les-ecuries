16c5cb535af565401739ec88ff7a0eaa8bf1dbc3
JulienBNT <julien.binet91@gmail.com>
2022-10-24 23:42:37 +0200

    :lipstick: added links-to-outside buttons

--- src/components/boutton/index.js
+++ src/components/boutton/index.js
@@ -5,9 +5,11 @@ function Boutton() {
 
 	return (
 	    <div className="boutton">
-        <button className='btn_fond_b' label="Réservation en ligne">
-           <p>Réservation en ligne</p>
-        </button>
+            <a href='https://pro.guestonline.fr/instabook/bookings/1kATjqd'>
+                <button className='btn_fond_b' label="Réservation en ligne">
+                    <p>Réservation en ligne</p>
+                </button>
+            </a>
         <button className='btn_fond_n' label="Contactez-nous">
             <p>Contactez-nous</p>
             <img src={vector} className="vector" alt="logo_contactez-nous" />
--- src/components/boutton_information/index.js
+++ src/components/boutton_information/index.js
@@ -6,14 +6,20 @@ function boutton_information() {
 
 	return (
 	    <div className="boutton_information">
-        <button className='btn_fond_n' label="Notre carte">
-           <p>Notre carte</p>
-           <img src={vector_1} className="vector_1" alt="logo_Notre carte" />
-        </button>
+            <a href='http://lesecuries-bar.com/wp-content/uploads/2015/06/Menu-Les-E%CC%81curies-Site.pdf'>
+                <button className='btn_fond_n' label="Notre carte">
+                    <p>Notre carte</p>
+                    <img src={vector_1} className="vector_1" alt="logo_Notre carte" />
+                </button>
+            </a>
+
+
+        <a href='https://www.facebook.com/lesecuriesbar/events'>
         <button className='btn_fond_n' label="Programmation">
             <p>Programmation</p>
             <img src={vector_2} className="vector_2" alt="logo_Programmation" />
         </button>
+        </a>
       </div>
 
 	)
