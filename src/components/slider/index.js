5ef0741990d07f66343c9b28ae3defcd87d21a59
Emerson452 <emerson452pro@gmail.com>
2022-11-25 14:42:11 +0100

    :construction::poop: test of a slider

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,18 +1,34 @@
-import './index.css'
+import "./index.css";
+import { Slide } from "react-slideshow-image";
 
-import image_3_slider from'../../assets/image_3.png';
-import image_4_slider from'../../assets/image_4.png';
-import image_5_slider from'../../assets/image_5.png';
+import image_3_slider from "../../assets/image_3.png";
+import image_4_slider from "../../assets/image_4.png";
+import image_5_slider from "../../assets/image_5.png";
 
-import arrow_left from '../../assets/arrow-left.png';
-import arrow_right from '../../assets/arrow-right.png';
+import arrow_left from "../../assets/arrow-left.png";
+import arrow_right from "../../assets/arrow-right.png";
 
 function Slider() {
+  const properties = {
+    duration: 5000,
+    transitionDuration: 500, 
+    infinite: true,
+    indicators: true,
+    arrows: true,
+  };
 
-	return (
+  return (
     <div className="slider">
       <div className="container-top-slider">
-        <img src={image_5_slider} alt="image_slider" />
+        <Slide {...properties}>
+          <div className="each-slide">
+            <div><img src={image_3_slider} alt="image_slider" /></div>
+          </div>
+          <div className="each-slide">
+          <div><img src={image_4_slider} alt="image_slider" /></div>
+          </div>
+
+        </Slide>
       </div>
 
       <div className="container-bot-slider">
@@ -26,12 +42,16 @@ function Slider() {
         </div>
 
         <div className="navigation">
-          <button><img src={arrow_left} alt="arrow_left" /></button>
-          <button><img src={arrow_right} alt="arrow_right" /></button>
+          <button>
+            <img src={arrow_left} alt="arrow_left" />
+          </button>
+          <button>
+            <img src={arrow_right} alt="arrow_right" />
+          </button>
         </div>
       </div>
     </div>
-	)
+  );
 }
 
-export default Slider
+export default Slider;
